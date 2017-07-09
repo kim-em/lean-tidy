a96b456d1089ffa3b87ff35383550c7b67af2bc3
Scott Morrison <scott.morrison@anu.edu.au>
2017-07-09 17:18:14 -0500

    a little work on chain

--- src/tidy/chain.lean
+++ src/tidy/chain.lean
@@ -7,9 +7,13 @@ import .if_then_else
 open tactic
 open nat
 
+-- FIXME how do we set options? It's annoying to have to pass a configuration everywhere.
+
 structure chain_cfg := 
-  ( max_steps   : nat  := 500 )
-  ( trace_steps : bool := ff )
+  ( max_steps     : nat  := 500 )
+  ( trace_steps   : bool := ff )
+  ( fail_on_loop  : bool := ff )
+  ( trace_on_loop : bool := tt )
 
 meta def hash_target : tactic string :=
 (done >> pure "no goals") <|>
@@ -25,26 +29,28 @@ private meta structure chain_progress { α : Type } :=
   ( remaining_tactics : list (tactic α) )
   ( hashes            : list string )
 
-meta def guard_progress { α : Type } ( hashes : list string ) ( t : tactic α ) : tactic ((list string) × α) :=
-do r ← t,
-   h ← hash_target,
-   guard ¬ hashes.mem h <|> trace "chain tactic detected looping" >> failed,
-   pure ((h :: hashes), r)
-
 private meta def chain' { α : Type } [ has_to_format α ] ( cfg : chain_cfg ) ( tactics : list (tactic α) ) : chain_progress → tactic (list α)
 | ⟨ 0,      results, _, hashes ⟩ := trace (format!"... chain tactic exceeded iteration limit {cfg.max_steps}") >>
                                      trace results.reverse >> 
-                                     trace hashes.reverse >>
                                      failed   
 | ⟨ _,      results, [], _ ⟩     := (pure results)
 | ⟨ succ n, results, t :: ts, hashes ⟩ :=
     if_then_else done
       (pure results)
       (do if cfg.trace_steps then trace format!"trying chain tactic #{tactics.length - ts.length}" else skip,
-          dependent_if_then_else (guard_progress hashes t)
-            (λ result, do (if cfg.trace_steps then trace format!"succeeded with result: {result.2}" else skip),  
-                          (chain' ⟨ n, result.2 :: results, tactics, result.1 ⟩ ))
-            (chain' ⟨ succ n, results, ts, hashes ⟩)
+          r ← t | /- tactic t failed, continue down the list -/ (chain' ⟨ succ n, results, ts, hashes ⟩),
+          h ← hash_target,
+          if hashes.mem h then 
+            /- we've run into a loop -/
+            do if cfg.fail_on_loop || cfg.trace_on_loop then trace "chain tactic detected looping" else skip,
+               if cfg.fail_on_loop then
+                 trace results.reverse >> failed
+               else 
+                 /- continue down the list -/
+                 (chain' ⟨ succ n, results, ts, hashes ⟩)
+          else 
+            do (if cfg.trace_steps then trace format!"succeeded with result: {r}" else skip),  
+                (chain' ⟨ n, r :: results, tactics, h :: hashes ⟩ )
       )
 
 meta def chain { α : Type } [ has_to_format α ] 
