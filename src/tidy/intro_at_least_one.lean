db156f89dc76083c1db9ba8f99707fbf08dbcd50
Scott Morrison <scott@tqft.net>
2017-08-02 15:17:50 +1000

    tidy

--- src/tidy/intro_at_least_one.lean
+++ src/tidy/intro_at_least_one.lean
@@ -4,12 +4,6 @@
 
 open tactic
 
--- Applies a list of tactics in turn, always succeeding.
-meta def try_list : list (tactic unit) → tactic unit 
-| list.nil  := skip
-| (t :: ts) := try t >> try_list ts
-
--- Applies a list of tactics in turn, succeeding if at least one succeeds.
-meta def at_least_one : list (tactic unit) → tactic unit
-| list.nil  := fail "at_least_one tactic failed, no more tactics"
-| (t :: ts) := (t >> try_list ts) <|> at_least_one ts
+meta def intro_at_least_one : tactic unit :=
+do l ← intros,
+   guard (¬ l.empty)
