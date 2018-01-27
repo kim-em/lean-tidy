ccd29b7f27c8adc9ffdad69003c5e2110dfa78a2
Scott Morrison <scott@tqft.net>
2018-01-27 20:56:53 +1100

    Restoring timing

--- src/tidy/timing.lean
+++ src/tidy/timing.lean
@@ -1,23 +1,24 @@
 
--- import system.io
+import system.io
 
--- open tactic
+open tactic
 
--- universe variables u
+universe variables u
 
--- -- There's apparently about a 1/5th of a second overhead here...
--- meta def time_in_nanos : tactic ℕ :=
--- do time ← tactic.run_io (λ [ioi : io.interface], @io.cmd ioi { cmd := "gdate", args := [ "+%s%N" ] } ),
---    pure time.to_nat
+-- There's apparently about a 1/5th of a second overhead here...
+meta def time_in_nanos : tactic ℕ :=
+do time ← tactic.unsafe_run_io (io.cmd { cmd := "gdate", args := [ "+%s%N" ] }),
+   pure time.to_nat
 
--- meta def time_tactic { α : Type } ( t : tactic α ) : tactic (α × ℕ) :=
--- do time_before ← time_in_nanos,
---    r ← t,
---    time_after ← time_in_nanos,
---    pure (r, time_after - time_before)
+-- measure in millis
+meta def time_tactic { α : Type } ( t : tactic α ) : tactic (α × ℕ) :=
+do time_before ← time_in_nanos,
+   r ← t,
+   time_after ← time_in_nanos,
+   pure (r, (time_after - time_before) / 1000000)
 
--- -- lemma f : 1 = 1 := 
--- -- begin
--- -- (time_tactic skip) >>= trace,
--- -- simp     
--- -- end
+lemma f : 1 = 1 := 
+begin
+(time_tactic skip) >>= trace,
+simp     
+end
