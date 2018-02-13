f755a5fa586a1e214391445f3994da181cfbe095
Scott Morrison <scott@tqft.net>
2018-02-13 17:08:27 +1100

    obviously

--- src/tidy/tidy.lean
+++ src/tidy/tidy.lean
@@ -127,7 +127,7 @@ do
    else
      tactic.skip
 
-meta def obviously := tidy >> (tactic.done <|> `[rewrite_search_using `ematch])
+meta def obviously := try tidy >> (tactic.done <|> `[rewrite_search_using `ematch])
 
 notation `♮` := by reducible_abstract { smt_eblast }
 notation `♯`  := by reducible_abstract { obviously }
