d8ce3a363ddf380a5d7850dcea1332a9a498a2cc
Scott Morrison <scott@tqft.net>
2018-03-08 22:38:40 -0700

    updating tests

--- test/chain.lean
+++ test/chain.lean
@@ -29,12 +29,12 @@ end
 
 def chain_test_loop_detection : 1 = 1 :=
 begin
-  chain [ skip, interactive_simp ] {}
+  chain [ skip, interactive_simp ] { fail_on_loop := tt } 
 end
 
 def chain_test_loop_detection' : 1 = 1 :=
 begin
-  chain [ skip, interactive_simp ] { allowed_collisions := 5, trace_steps := tt }
+  chain [ skip, interactive_simp ] { fail_on_loop := tt, allowed_collisions := 5, trace_steps := tt }
 end
 
 end tidy.test
