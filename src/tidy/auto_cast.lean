d6a1b8e4182662ea705ea275aa8d85e650904d37
Scott Morrison <scott.morrison@anu.edu.au>
2018-03-27 14:19:06 +1100

    auto_cast notation

--- src/tidy/auto_cast.lean
+++ src/tidy/auto_cast.lean
@@ -8,4 +8,4 @@ open tactic
 
 @[reducible] def {u} auto_cast {α β : Sort u} {h : α = β} (a : α) := cast h a
 @[simp] lemma {u} auto_cast_identity {α : Sort u} (a : α) : @auto_cast α α (by smt_ematch) a = a := begin unfold auto_cast, trivial, end
-notation `⟦` p `⟧` := @auto_cast _ _ (by smt_ematch) p
+notation `⟬` p `⟭` := @auto_cast _ _ (by smt_ematch) p
