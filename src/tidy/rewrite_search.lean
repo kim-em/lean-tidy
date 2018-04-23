50bcde0ccbfb61b7d9ae9897983ae79ebf9a3452
Scott Morrison <scott@tqft.net>
2018-04-23 22:35:14 +1000

    allowing some extra max distance

--- src/tidy/rewrite_search.lean
+++ src/tidy/rewrite_search.lean
@@ -101,7 +101,7 @@ do
 structure rewrite_search_config :=
   (trace        : bool := ff)
   (trace_result : bool := ff)
-  (max_extra_distance : ℕ := 5)
+  (max_extra_distance : ℕ := 12)
 
 meta def attempt_refl (lhs rhs : expr) : tactic unit :=
 lock_tactic_state $
