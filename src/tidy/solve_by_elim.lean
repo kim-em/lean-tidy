760dbe9333d2962cbf86af5d8751139cb54b81c6
Scott Morrison <scott@tqft.net>
2018-04-25 21:49:00 +1000

    removing solve_by_elim and its in favour of mathlib's versions

--- src/tidy/solve_by_elim.lean
+++
@@ -1,6 +0,0 @@
-import tactic.interactive
-
--- TODO port to mathlib
-meta def solve_by_elim' (discharger : tactic unit := tactic.done) (asms : option (list expr) := none) : opt_param ℕ 3 → tactic unit
-| 0     := tactic.done
-| (n+1) := discharger <|> (tactic.interactive.apply_assumption asms $ solve_by_elim' n)
