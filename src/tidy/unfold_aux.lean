ac6b9465d911c343e0140d8905e5db684938e2e4
Scott Morrison <scott@tqft.net>
2018-08-18 16:28:34 +0200

    oops, missing file

---
+++ src/tidy/unfold_aux.lean
@@ -0,0 +1,15 @@
+open tactic
+
+meta def expr.names_with_prefix (pre : name) (r : expr) : list name := 
+r.fold [] $ λ e _ l,
+  match e with
+  | expr.const n _ := if n.get_prefix = pre then insert n l else l
+  | _ := l
+  end
+
+meta def unfold_aux : tactic unit :=
+do tgt ← target,
+   name ← decl_name,
+   let to_unfold := tgt.names_with_prefix name,
+   guard (to_unfold ≠ []),
+   dunfold to_unfold tgt >>= change
