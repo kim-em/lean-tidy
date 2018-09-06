a7d9447da458593fa70ec4823a3531216d275259
Scott Morrison <scott.morrison@anu.edu.au>
2018-09-06 14:40:58 +1000

    investigating kabstract

---
+++ src/tidy/rewrite_all_2.lean
@@ -0,0 +1,36 @@
+open tactic
+
+def f (x : ℕ) := 2
+
+example : f 1 = f 2 :=
+begin
+(do t ← target,
+   trace t,
+   a ← to_expr ``(f _),
+   trace a,
+   ty ← infer_type a,
+   trace ty,
+   k ← kabstract t a transparency.reducible ff,
+   trace k,
+   skip),
+   refl
+end
+
+
+constant v : ℕ
+
+example : 1 = 1 :=
+begin
+do t ← target,
+   let a := `(1),
+   ty ← infer_type a,
+   trace t,
+   k ← kabstract t a,
+   trace k,
+   v ← mk_const `v,
+   let w := expr.local_const `w `w binder_info.default ty,
+   trace (k.instantiate_var v),
+   trace (k.instantiate_var w),
+   skip
+--    trace v,
+end
