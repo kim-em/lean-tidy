7f7e72b7c39ebe9ee677a5c7ec2a3a6683a97d4d
Keeley Hoek <keeley@hoek.io>
2018-10-21 03:55:24 +1100

    Rudimentary "generic proof unit" explaining

--- src/tidy/rewrite_search/core/explain.lean
+++ src/tidy/rewrite_search/core/explain.lean
@@ -10,8 +10,15 @@ def how.to_tactic (rule_strings : list string) : how → option string
 | (how.rewrite index s location) := some ("nth_rewrite" ++ (match s with | side.L := "_lhs" | side.R := "_rhs" end) ++ " " ++ to_string location ++ " " ++ (rule_strings.nth index).iget)
 | _ := none
 
+def how.to_tactic_side (s : side) (rule_strings : list string) : how → option string
+| (how.rewrite index _ location) := some ("nth_rewrite" ++ (match s with | side.L := "_lhs" | side.R := "_rhs" end) ++ " " ++ to_string location ++ " " ++ (rule_strings.nth index).iget)
+| _ := none
+
 meta def explain_proof (rule_strings : list string) (steps : list how) : string :=
 string.intercalate ",\n" (steps.map $ λ s : how, (s.to_tactic rule_strings).to_list).join
+
+meta def explain_proof' (ss : side) (rule_strings : list string) (steps : list how) : string :=
+string.intercalate ",\n" (steps.map $ λ s : how, (s.to_tactic_side ss rule_strings).to_list).join
 
 def how.concisely (rule_strings : list string) : how → option string
 | (how.rewrite index side location) := some (rule_strings.nth index).iget
@@ -31,32 +38,43 @@ do
 
 meta def pp_rules (rs : list (expr × bool)) : tactic (list string) := rs.mmap (λ p, (do pp ← pretty_print p.1, return (if p.2 then ("←" ++ pp) else pp)))
 
-meta def explain_search_result' (cfg : config) (proof : expr) (steps : list how) : tactic string := do
-  if cfg.trace then do
-  pp ← pretty_print proof,
-  trace format!"rewrite_search found proof:\n{pp}"
-  else skip,
+meta def explain_proof_unit (cfg : config) (unit : proof_unit) : tactic string := do
+  let steps := if unit.trans_start.is_none then unit.hows else unit.hows.reverse,
   rules_strings ← pp_rules cfg.rs,
   let rewrites := (steps.map $ λ s, match s with
                                   | how.rewrite index _ _ := [(cfg.rs.nth index).iget]
                                   | _ := []
                                   end).join,
   explanation ← (do
+    -- FIXME `check_if_simple_rewrite_succeeds` won't work anymore, since it was designed
+    -- for when we were considering the whole goal at once. Probably we can do two things,
+    -- first give it a go to see if it solves the whole thing, then if that fails proceed as
+    -- normal, and try to use the same thing on each proof unit, to simplify it. We could leave
+    -- a TODO about how we could try to merge adjacent proof units or something, but currently
+    -- (once implemented) only try the "most coarse" and "most fine" levels of simplication, not
+    -- anywhere in between.
+
+    -- Also the "sides" of the part above are probably broken now too.
     needs_refl ← check_if_simple_rewrite_succeeds rewrites,
     return (explain_proof_concisely rules_strings steps needs_refl)
-  ) <|> return (explain_proof rules_strings steps),
-  if cfg.trace_result then trace explanation
-  else skip,
+  ) <|> return (explain_proof' (if unit.trans_start.is_none then side.L else side.R) rules_strings steps),
   return explanation
 
-meta def explain_search_result (cfg : config) (proof : expr) (steps : list proof_unit) : tactic string := do
-  -- FIXME introduce suppose for general `proof_unit` structures
-  match steps with
-  | [u] := if u.trans_start.is_none then explain_search_result' cfg proof u.hows
-    else explain_search_result' cfg proof u.hows.reverse
-  | [u1, u2] := if ¬(u1.trans_start.is_none ∧ u2.trans_start.is_some) then return "<???>"
-    else explain_search_result' cfg proof (u1.hows ++ u2.hows.reverse)
-  | _ := return "<???>"
-  end
+meta def explain_search_result (cfg : config) (proof : expr) (units : list proof_unit) : tactic string := do
+  if cfg.trace then do
+    pp ← pretty_print proof,
+    trace format!"rewrite_search found proof:\n{pp}"
+  else skip,
+
+  -- FIXME drop the "transitivity" when this is the last unit, since it will be redundant.
+  explanation ← to_string <$> list.join <$> units.mmap (λ u, do
+    unit_expl ← explain_proof_unit cfg u,
+    return $ (u.trans_start.to_list.map $ λ n, "transitivity " ++ n) ++ [unit_expl]
+  ),
+
+  if cfg.trace_result then trace explanation
+  else skip,
+
+  return explanation
 
 end tidy.rewrite_search
