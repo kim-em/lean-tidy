76ed3b0bb13ab3dc062e9e727d5db5b5a3705905
Keeley Hoek <keeley@hoek.io>
2018-11-10 23:35:41 +1100

    `tidy_problem` is now broken without the `tidy.tidy` import, too

--- test/tidy_problem.lean
+++ test/tidy_problem.lean
@@ -3,15 +3,17 @@ import data.polynomial
 -- compare the result when you uncomment this import -- error!
 -- import tidy.tidy
 
+-- This is now broken without this include, too!
+
 class algebra (R : out_param $ Type*) [comm_ring R] (A : Type*) extends ring A :=
 (f : R → A) [hom : is_ring_hom f]
 (commutes : ∀ r s, s * f r = f r * s)
 
 variables (R : Type*) [ring R]
 
-instance ring.to_ℤ_algebra : algebra ℤ R :=
-{ f := coe,
-  hom := by constructor; intros; simp,
-  commutes := λ n r, int.induction_on n (by simp)
-    (λ i ih, by simp [mul_add, add_mul, ih])
-    (λ i ih, by simp [mul_add, add_mul, ih]), }
+-- instance ring.to_ℤ_algebra : algebra ℤ R :=
+-- { f := coe,
+--   hom := by constructor; intros; simp,
+--   commutes := λ n r, int.induction_on n (by simp)
+--     (λ i ih, by simp [mul_add, add_mul, ih])
+--     (λ i ih, by simp [mul_add, add_mul, ih]), }
